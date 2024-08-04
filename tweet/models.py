ef9595b13b205663ab1607ac27ce1e965fdb2c3a
Dipayan Sarkar <129957226+Dipayan23@users.noreply.github.com>
2024-08-04 10:48:21 +0530

    Done

--- tweet/models.py
+++ tweet/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Tweet(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     text = models.TextField(max_length=200)
-    photo = models.ImageField(upload_to='photos/',blank=True,null=True)
+    photo = models.ImageField(upload_to='media/photos/',blank=True,null=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
